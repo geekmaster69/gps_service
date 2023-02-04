6a792ddee002a544f5a5bdc30559ea358ff1da3e
geekmaster69 <emmanuelmosa@gmail.com>
2023-02-03 18:57:01 -0600

    First Commit

--- app/src/main/java/com/example/locationservicegps/MainActivity.kt
+++ app/src/main/java/com/example/locationservicegps/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.locationservicegps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.example.locationservicegps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            0
+        )
+
+        binding.btnStart.setOnClickListener {
+            Intent(applicationContext, LocationService::class.java).apply {
+                action = LocationService.ACTION_STAR
+                startService(this)
+            }
+
+        }
+
+        binding.btnStop.setOnClickListener {
+            Intent(applicationContext, LocationService::class.java).apply {
+                action = LocationService.ACTION_STOP
+                startService(this)
+            }
+
+        }
     }
 }
